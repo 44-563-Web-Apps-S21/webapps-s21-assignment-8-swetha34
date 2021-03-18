0a321a2ca0984c9ce543504fc6f8e1bdd38c3d52
swetha34 <ghanaswetha743@gmail.com>
2021-03-17 23:00:46 -0500

    bonus final

---
+++ bonus.js
@@ -0,0 +1,44 @@
+const http = require('http')    //Pull in a useful node package
+
+
+const hostname = process.env.hostname || '127.0.0.1'    //get our ip address from the environment
+const port = process.env.port || 3001               //and the port
+
+const server =
+    http.createServer(            //Creates the response loop
+        (req, res) => {               //Anonymous function to handle the request
+            console.log("Request recieved")
+            // console.log(req.url)
+            // Build a fake url so we can get the search parameters using a URL object
+            fake_url = "https://fake.com/path" + req.url
+            const url = new URL(fake_url)
+            if (req.method === 'GET') {
+
+                //   console.log("Look for query parameter data: " + search_params.get("data"))
+                const advices = ["Come to party",
+                    "You will hve many friends.",
+                    "Today climate is so cool.",
+                    "Have a good day.",
+                    "You will be having good time.",
+                    "Thanks to giving some money.",
+                    "You will have very good luck today."
+                ]
+                let randomIndex = 1
+                // Process the queries here
+                res.statusCode = 200      //code for OK
+                res.setHeader('Content-Type', 'text/plain')
+                res.write(`${advices[randomIndex]}`)
+                res.end();
+
+            } else {
+                console.log("Status 404")
+                res.statusCode = 404;
+                res.end();
+            }
+
+        }
+    )
+
+server.listen(port, hostname, () => {   //Start the server
+    console.log(`Server running at http://${hostname}:${port}/`)  //Log the request
+})
